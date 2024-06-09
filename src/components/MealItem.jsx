339136d090960f0d2b1f1b5724aa025b9919b6b2
Rohit Singh <rohitsinghpb4@gmail.com>
2024-06-09 21:03:36 +0530

    image issue fix

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -2,7 +2,10 @@ function MealItem({ meal }) {
     return (
         <li className="meal-item">
             <article>
-                <img src={meal.image} alt={meal.name} />
+                <img
+                    src={`http://localhost:1000/${meal.image}`}
+                    alt={meal.name}
+                />
                 <div>
                     <h3>{meal.name}</h3>
                     <p className="meal-item-price">{meal.price}</p>
