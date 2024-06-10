8201c5f4f61db700abaa0adda328ba7ac3b63e60
Rohit Singh <rohitsinghpb4@gmail.com>
2024-06-10 18:56:52 +0530

    CartItem Component is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import Header from "./components/Header";
-import Meals from "./components/Meals";
+import Cart from "./components/Cart.jsx";
+import Checkout from "./components/Checkout.jsx";
+import Header from "./components/Header.jsx";
+import Meals from "./components/Meals.jsx";
 import { CartContextProvider } from "./store/CartContext.jsx";
+import { UserProgressContextProvider } from "./store/UserProgressContext.jsx";
 function App() {
     return (
-        <CartContextProvider>
-            <Header />
-            <Meals />
-        </CartContextProvider>
+        <UserProgressContextProvider>
+            <CartContextProvider>
+                <Header />
+                <Meals />
+                <Cart />
+                <Checkout />
+            </CartContextProvider>
+        </UserProgressContextProvider>
     );
 }
 
--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,35 +1,57 @@
-import { useContext } from "react";
+import { useContext } from 'react';
 
-import Modal from "./ui/Modals.jsx";
-import CartContext from "../store/CartContext.jsx";
-import currencyFormatter from "../Util/formatting.js";
-import Button from "./ui/Button.jsx";
+import Modal from './UI/Modal.jsx';
+import CartContext from '../store/CartContext.jsx';
+import Button from './UI/Button.jsx';
+import { currencyFormatter } from '../util/formatting.js';
+import UserProgressContext from '../store/UserProgressContext.jsx';
+import CartItem from './CartItem.jsx';
 
-function Cart() {
-    const cartCtx = useContext(CartContext);
+export default function Cart() {
+  const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
 
-    const cartTotal = cartCtx.items.reduce(
-        (totalPrice, item) => (totalPrice = item.quantity * item.price),
-        0
-    );
+  const cartTotal = cartCtx.items.reduce(
+    (totalPrice, item) => totalPrice + item.quantity * item.price,
+    0
+  );
 
-    return (
-        <Modal className="cart">
-            <h2>Your Cart</h2>
-            <ul>
-                {cartCtx.items.map((item) => {
-                    <li key={item.id}>
-                        {item.name} - {item.quantity}
-                    </li>;
-                })}
-            </ul>
-            <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
-            <p className="modal-actions">
-                <Button textOnly>Close</Button>
-                <Button>Go To Checkout</Button>
-            </p>
-        </Modal>
-    );
+  function handleCloseCart() {
+    userProgressCtx.hideCart();
+  }
+
+  function handleGoToCheckout() {
+    userProgressCtx.showCheckout();
+  }
+
+  return (
+    <Modal
+      className="cart"
+      open={userProgressCtx.progress === 'cart'}
+      onClose={userProgressCtx.progress === 'cart' ? handleCloseCart : null}
+    >
+      <h2>Your Cart</h2>
+      <ul>
+        {cartCtx.items.map((item) => (
+          <CartItem
+            key={item.id}
+            name={item.name}
+            quantity={item.quantity}
+            price={item.price}
+            onIncrease={() => cartCtx.addItem(item)}
+            onDecrease={() => cartCtx.removeItem(item.id)}
+          />
+        ))}
+      </ul>
+      <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
+      <p className="modal-actions">
+        <Button textOnly onClick={handleCloseCart}>
+          Close
+        </Button>
+        {cartCtx.items.length > 0 && (
+          <Button onClick={handleGoToCheckout}>Go to Checkout</Button>
+        )}
+      </p>
+    </Modal>
+  );
 }
-
-export default Cart;
