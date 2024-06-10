08d4b655a0e614fc7e2729c8bd063b03b1ed552f
Rohit Singh <rohitsinghpb4@gmail.com>
2024-06-10 18:59:12 +0530

    input component is added

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,35 +1,31 @@
-import { useContext } from "react";
-import currencyFormatter from "../Util/formatting.js";
-import Button from "./ui/Button.jsx";
-import CartContext from "../store/CartContext.jsx";
+import { useContext } from 'react';
 
-function MealItem({ meal }) {
-    const cartCtx = useContext(CartContext);
+import { currencyFormatter } from '../util/formatting.js';
+import Button from './UI/Button.jsx';
+import CartContext from '../store/CartContext.jsx';
 
-    function handleAddMealToCart() {
-        cartCtx.addItem(meal);
-    }
+export default function MealItem({ meal }) {
+  const cartCtx = useContext(CartContext);
 
-    return (
-        <li className="meal-item">
-            <article>
-                <img
-                    src={`http://localhost:1000/${meal.image}`}
-                    alt={meal.name}
-                />
-                <div>
-                    <h3>{meal.name}</h3>
-                    <p className="meal-item-price">
-                        {currencyFormatter.format(meal.price)}
-                    </p>
-                    <p className="meal-item-description">{meal.description}</p>
-                </div>
-                <p className="meal-item-actions">
-                    <Button onClick={handleAddMealToCart}>Add to Cart</Button>
-                </p>
-            </article>
-        </li>
-    );
+  function handleAddMealToCart() {
+    cartCtx.addItem(meal);
+  }
+
+  return (
+    <li className="meal-item">
+      <article>
+        <img src={`http://localhost:3000/${meal.image}`} alt={meal.name} />
+        <div>
+          <h3>{meal.name}</h3>
+          <p className="meal-item-price">
+            {currencyFormatter.format(meal.price)}
+          </p>
+          <p className="meal-item-description">{meal.description}</p>
+        </div>
+        <p className="meal-item-actions">
+          <Button onClick={handleAddMealToCart}>Add to Cart</Button>
+        </p>
+      </article>
+    </li>
+  );
 }
-
-export default MealItem;
--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,29 +1,30 @@
-import { useEffect } from "react";
-import { useState } from "react";
-import MealItem from "./MealItem.jsx";
+import { useState, useEffect } from 'react';
 
-function Meals() {
-    const [loadedMeals, setLoadedMeals] = useState([]);
-    useEffect(() => {
-        async function fetchMeals() {
-            const response = await fetch("http://localhost:1000/meals");
+import MealItem from './MealItem.jsx';
 
-            if (!response.ok) {
-                //...
-            }
-            const meals = await response.json();
-            setLoadedMeals(meals);
-        }
-        fetchMeals();
-    }, []);
+export default function Meals() {
+  const [loadedMeals, setLoadedMeals] = useState([]);
 
-    return (
-        <ul id="meals">
-            {loadedMeals.map((meal) => (
-                <MealItem key={meal.id} meal={meal} />
-            ))}
-        </ul>
-    );
+  useEffect(() => {
+    async function fetchMeals() {
+      const response = await fetch('http://localhost:3000/meals');
+
+      if (!response.ok) {
+        // ...
+      }
+
+      const meals = await response.json();
+      setLoadedMeals(meals);
+    }
+
+    fetchMeals();
+  }, []);
+
+  return (
+    <ul id="meals">
+      {loadedMeals.map((meal) => (
+        <MealItem key={meal.id} meal={meal} />
+      ))}
+    </ul>
+  );
 }
-
-export default Meals;
