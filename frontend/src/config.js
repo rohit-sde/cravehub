3b3db87340f6c2d64dfc02315f545dc863963b67
Rohit Singh <rohitsinghpb4@gmail.com>
2024-12-14 22:44:10 +0530

    yfghv

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,5 @@
-const backendUrl = 'http://localhost:5064'?.replace(/\/$/, '');
+const backendUrl = 'http://localhost:5063'?.replace(/\/$/, '');
 
 export const CONFIG = {
   apiUrl: process.env.REACT_APP_NODE_ENV === 'production' ? '' : backendUrl,
 };
-
-console.log(process.env);
