c8e3468473dca24e587dbbde0946e754f1291a08
Rohit Singh <rohitsinghpb4@gmail.com>
2024-12-14 22:28:02 +0530

    dwe

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,7 @@
-const backendUrl = 'http://localhost:5063'?.replace(/\/$/, '');
+const backendUrl = 'http://localhost:5064'?.replace(/\/$/, '');
 
 export const CONFIG = {
   apiUrl: process.env.REACT_APP_NODE_ENV === 'production' ? '' : backendUrl,
 };
+
+console.log(process.env);
